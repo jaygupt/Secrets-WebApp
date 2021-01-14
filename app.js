a1e2c4f493541929a2761d6de7729ab03f2b30b7
Jay Gupta <jaygupt1973@gmail.com>
2021-01-14 06:37:45 -0800

    Add encryption, .gitignore, and .env files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config(); 
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const ejs = require("ejs"); 
 const mongoose = require("mongoose"); 
+const encrypt = require("mongoose-encryption");  
 
 const app = express(); 
 
@@ -19,12 +21,14 @@ mongoose.connect("mongodb://localhost:27017/userDB",
   }
 ); 
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String 
-};
+}); 
 
-const User = mongoose.model("User", userSchema); 
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); 
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home"); 
@@ -52,6 +56,24 @@ app.post("/register", function(req, res){
     }
   }); 
 });
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password; 
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          // they are a valid user
+          res.render("secrets"); 
+        }
+      }
+    }
+  });
+}); 
 
 app.listen(3000, function(){
   console.log("Server started on port 3000!"); 
